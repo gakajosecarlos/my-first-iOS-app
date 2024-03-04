6a18d055b6a5bce5ac9eede8cf72455d11c2ca77
carlos galvan <gakajosecarlos@gmail.com>
2024-03-03 20:39:19 -0600

    Initial configuration

---
+++ My First iOS App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My First iOS App
+//
+//  Created by Jose Carlos Galvan Kamey on 3/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
